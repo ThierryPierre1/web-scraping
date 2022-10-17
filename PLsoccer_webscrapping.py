f0aa18f7dc8cf42be49653a53a132e321b8e91f3
Tpierre31 <thierry.pierre@stonybrook.edu>
2022-10-17 01:44:46 -0400

    Commit

---
+++ PLsoccer_webscrapping.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+page = requests.get('https://www.premierleague.com/tables')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+print(soup.prettify())
+
+table = soup.find('tbody', class_='tableBodyContainer isPL')
+print(table)
+output_titles = []
+
+for teams in table:
+    print(teams.text)
+    data = table.text
+    standings = data.strip() 
+    record = standings.replace('\n','')
+    plStandings = record.replace(' ','')
+len(plStandings)
+
+list1 = plStandings
+
+dictionary = {'name': list1} 
+
+df = pd.DataFrame({'name': [list1]})
+df
+
+df.to_csv('/Users/thierrypierre/Desktop/PremierLeague_standing')
